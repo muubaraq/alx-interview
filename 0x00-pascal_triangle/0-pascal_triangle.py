6c30b4f25cc1f735191c317eae4f330d2a6d0205
mubaraq <paynedaze@gmail.com>
2022-12-19 22:30:02 +0100

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    result = []
+    for i in range(n):
+        if i == 0:
+            result.append([1])
+        elif i == 1:
+            result.append([1, 1])
+        else:
+            row = [1]
+            for j in range(1, i):
+                row.append(result[i-1][j-1] + result[i-1][j])
+            row.append(1)
+            result.append(row)
+    return result
